aead3ebe7f0c749ea723b62b74aa24472b36c9e0
Subashini <subashini3910@gmail.com>
2021-11-22 19:39:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App Componentname="Functional"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
